727528d6a9d3066da4911cb99bbf9d6c711c2641
Javier Lorenzo Carrion <javierlorenzocarrion@gmail.com>
2025-03-27 15:18:18 +0000

    refactor: inline the variable in toUpperCamelCase function

--- src/core/camelCaseV1.ts
+++ src/core/camelCaseV1.ts
@@ -1,4 +1,3 @@
 export function toUpperCamelCaseV1(text: string): string {
-    let convertedText = text.charAt(0).toUpperCase() + text.slice(1);
-    return convertedText;
+    return text.charAt(0).toUpperCase() + text.slice(1);
 }
