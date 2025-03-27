88a56636e673c0fbab27a7c7158735fc4a37687a
Javier Lorenzo Carrion <javierlorenzocarrion@gmail.com>
2025-03-27 19:32:31 +0000

    feat: implement toUpperCamelCase function to return the first letter in uppercase of a word

--- src/core/camelCaseV2.ts
+++ src/core/camelCaseV2.ts
@@ -1,3 +1,3 @@
 export function toUpperCamelCaseV2(text: string): string{
-    return text;
+    return text.replace(text.charAt(0), text.charAt(0).toUpperCase());
 }
--- src/tests/camelCaseV2.test.ts
+++ src/tests/camelCaseV2.test.ts
@@ -4,4 +4,10 @@ describe("To upper camel version 2 case should", () => {
     it("return a string: when the initial sentence is:", () => {
         expect(toUpperCamelCaseV2("")).toBe("");
     })
+    it("return a string:  when the initial sentence is: ", ()=>{
+        expect(toUpperCamelCaseV2(" ")).toBe(" ");
+    });
+    it("return a string: Hola when the initial sentence is: hola", ()=>{
+        expect(toUpperCamelCaseV2("hola")).toBe("Hola");
+    });
 })
