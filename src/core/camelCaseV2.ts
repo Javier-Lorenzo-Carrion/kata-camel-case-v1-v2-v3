c15afc074925ec79aacbfa08ee6839eb8f87563b
Javier Lorenzo Carrion <javierlorenzocarrion@gmail.com>
2025-03-27 19:49:43 +0000

    refactor: inline firstLetter variable

--- src/core/camelCaseV2.ts
+++ src/core/camelCaseV2.ts
@@ -1,8 +1,7 @@
 export function toUpperCamelCaseV2(text: string): string{
-    let firstLetter: string = text.charAt(0).toUpperCase();
     let restOfLetters: string = "";
     for(let i: number = 1; i < text.length; i++){
         restOfLetters = restOfLetters + text.charAt(i).toLowerCase();
     }
-    return firstLetter + restOfLetters;
+    return text.charAt(0).toUpperCase() + restOfLetters;
 }
