d1c813f27a01b8e7b136d2b2ecad963402cabb8d
Javier Lorenzo Carrion <javierlorenzocarrion@gmail.com>
2025-03-28 14:26:53 +0000

    refactor: eliminate duplicities files, compress all the files with code in one file and all the files with tests in one file and change their names

--- src/core/camelCase.ts
+++ src/core/camelCase.ts
@@ -9,3 +9,17 @@ export function toUpperCamelCaseV1(text: string): string {
     }
     return joinedText;
 }
+
+export function toUpperCamelCaseV2(text:string): string{
+    if(text === " ") return text;
+    let formatedText = "";
+    for (let i = 0; i < text.length; i++) {
+        if(text.charAt(i) === "-" || text.charAt(i) === " " || text.charAt(i) === "_"){
+            formatedText = formatedText + text.charAt(i+1).toUpperCase();
+            i++;
+        } else{
+            formatedText += text.charAt(i).toLowerCase();
+        }
+    }
+    return formatedText.charAt(0).toUpperCase() + formatedText.substring(1);
+}
---
+++ src/tests/camelCase.test.ts
@@ -0,0 +1,49 @@
+import {toUpperCamelCaseV1, toUpperCamelCaseV2} from "../core/camelCase";
+
+describe("To upper camel case version 1 should ", () => {
+    it("return a string: when the initial sentence is:", ()=>{
+        expect(toUpperCamelCaseV1("")).toBe("");
+    });
+    it("return a string: when the initial sentence is: ", ()=>{
+        expect(toUpperCamelCaseV1(" ")).toBe(" ");
+    });
+    it("return a string: Hola when the initial sentence is: hola", ()=>{
+        expect(toUpperCamelCaseV1("hola")).toBe("Hola");
+    });
+    it("return a string: Hola when the initial sentence is: HoLa", ()=>{
+        expect(toUpperCamelCaseV1("HoLa")).toBe("Hola");
+    });
+    it("return a string: SoyJavier when the initial sentence is: SOY-JAVIER", ()=>{
+        expect(toUpperCamelCaseV1("SOY-JAVIER")).toBe("SoyJavier");
+    });
+    it("return a string: HolaSoyJavier when the initial sentence is: hola_SOY-JAVIER", ()=>{
+        expect(toUpperCamelCaseV1("hola_SOY-JAVIER")).toBe("HolaSoyJavier");
+    });
+    it("return a string: HolaSoyJavier when the initial sentence is: hola SOY-_JAVIER", ()=>{
+        expect(toUpperCamelCaseV1("hola SOY-_JAVIER")).toBe("HolaSoyJavier");
+    });
+})
+
+describe("To upper camel case version 2 should", () => {
+    it("return a string: when the initial sentence is:", ()=>{
+        expect(toUpperCamelCaseV2("")).toBe("");
+    });
+    it("version 2 should return a string:  when the initial sentence is: ", ()=>{
+        expect(toUpperCamelCaseV2(" ")).toBe(" ");
+    });
+    it("return a string: Hola when the initial sentence is: hola", ()=>{
+        expect(toUpperCamelCaseV2("hola")).toBe("Hola");
+    });
+    it("return a string: Hola when the initial sentence is: HoLa", ()=>{
+        expect(toUpperCamelCaseV2("HoLa")).toBe("Hola");
+    });
+    it("return a string: SoyJavier when the initial sentence is: SOY-JAVIER", ()=>{
+        expect(toUpperCamelCaseV2("SOY-JAVIER")).toBe("SoyJavier");
+    });
+    it("return a string: HolaSoyJavier when the initial sentence is: hola_SOY-JAVIER", ()=>{
+        expect(toUpperCamelCaseV2("hola_SOY-JAVIER")).toBe("HolaSoyJavier");
+    });
+    it("return a string: HolaSoyJavier when the initial sentence is: hola SOY-_JAVIER", ()=>{
+        expect(toUpperCamelCaseV2("hola SOY-_JAVIER")).toBe("HolaSoyJavier");
+    });
+})
