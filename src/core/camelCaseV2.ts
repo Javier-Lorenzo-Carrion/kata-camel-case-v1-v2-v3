817675846544ea5543ead96301308db53e90a4dd
Javier Lorenzo Carrion <javierlorenzocarrion@gmail.com>
2025-03-28 14:01:36 +0000

    feat: implement toUpperCamelCase function to return the first letter in upper case and the rest of letters in lower case of each word in a sentence and eliminate hyphen and underscore and spaces

--- src/core/camelCaseV2.ts
+++ src/core/camelCaseV2.ts
@@ -2,7 +2,7 @@ export function toUpperCamelCaseV2(text:string): string{
     if(text === " ") return text;
     let formatedText = "";
     for (let i = 0; i < text.length; i++) {
-        if(text.charAt(i) === "-" || text.charAt(i) === " "){
+        if(text.charAt(i) === "-" || text.charAt(i) === " " || text.charAt(i) === "_"){
             formatedText = formatedText + text.charAt(i+1).toUpperCase();
             i++;
         } else{
